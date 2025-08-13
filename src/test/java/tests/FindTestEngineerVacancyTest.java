8f8db900a06608b1b9b4aa8d2b78d5ce3c495b56
Nikolay_Kossov <nkoss2019@gmail.com>
2025-08-13 16:06:08 +0200

    .

--- src/test/java/tests/FindTestEngineerVacancyTest.java
+++ src/test/java/tests/FindTestEngineerVacancyTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,11 @@ public class FindTestEngineerVacancyTest {
         open("https://commerzbank-poland.breezy.hr/");
         $$(".positions-container li").findBy(text("Test Engineer Intern")).click();
         $("#heroBackgroundColor").shouldHave(text("Test Engineer Intern"));
+        String finalUrl = WebDriverRunner.url();
+        System.out.println("Final URL: " + finalUrl);
+
+        // Если нужно добавить в Allure
+        io.qameta.allure.Allure.addAttachment("Open url", finalUrl);
+
     }
 }
