ba15136c4b039bcaeba14ab95d00b937460ed80f
Nikolay_Kossov <nkoss2019@gmail.com>
2025-08-13 15:58:47 +0200

    .

--- src/test/java/tests/ApplyToThePositionTest.java
+++ src/test/java/tests/ApplyToThePositionTest.java
@@ -1,11 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
