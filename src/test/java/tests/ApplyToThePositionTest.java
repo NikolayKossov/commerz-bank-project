d4bbd28195c39be23b675d081bb6bf1115b62e3e
Nikolay_Kossov <nkoss2019@gmail.com>
2025-08-17 16:37:46 +0200

    Applt test steps update

--- src/test/java/tests/ApplyToThePositionTest.java
+++ src/test/java/tests/ApplyToThePositionTest.java
@@ -1,21 +1,31 @@
 package tests;
 
-
 import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 @Epic("Job Application")
 @Feature("Application form")
+@Story("Open application form from vacancy page")
 @Owner("Nikolay Kossov")
 @Severity(SeverityLevel.CRITICAL)
+@DisplayName("Open application form from vacancy page")
 @Tag("simple")
 public class ApplyToThePositionTest extends TestBase {
+
     @Test
     void applyTest() {
-        open("https://commerzbank-poland.breezy.hr/p/904b265d84c001-test-engineer-intern-document-lifecycle--talentbank-2025");
-        $(".sidebar-container").$(byText("Apply To Position")).click();
+        step("Open vacancy page", () ->
+                open("https://commerzbank-poland.breezy.hr/p/904b265d84c001-test-engineer-intern-document-lifecycle--talentbank-2025")
+        );
+
+        step("Click 'Apply To Position' button", () ->
+                $(".sidebar-container").$(byText("Apply To Position")).click()
+        );
     }
 }
